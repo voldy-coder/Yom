5389ce989db093ef14a65782417d4b9f47a264dc
voldigoa46-del <voldigoa46@gmail.com>
2025-08-19 18:37:07 +0000

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -108,7 +108,7 @@ module.exports = {
     });
     const xyrene = {
       body: getLang("myPrefix", global.GoatBot.config.prefix, utils.getPrefix(event.threadID)),
-      attachment: await global.utils.getStreamFromURL("https://drive.google.com/uc?id=1a72QhBuup4I3IcYhDblJBe1D__fyIh4k")
+      attachment: await global.utils.getStreamFromURL("https://i.imgur.com/fP0OcDF.jpeg")
         };
     if (event.body && event.body.toLowerCase() === "prefix")
       return () => {
